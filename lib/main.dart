484f6ec306d0120d191b3182051ecd383b01866a
hasin-rahman <hasinisraq2004@gmail.com>
2025-03-10 13:29:15 -0400

    app code implimented

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,194 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return ChangeNotifierProvider(
+      create: (context) => GameState(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Card Matching Game",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: CardMatchingGame(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class GameState extends ChangeNotifier {
+  List<CardModel> _cards = [];
+  CardModel? _firstSelected;
+  CardModel? _secondSelected;
+  bool _isProcessing = false;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  GameState() {
+    _initializeGame();
+  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  void _initializeGame() {
+    List<String> icons = ['🍎', '🍌', '🍒', '🍇', '🍓', '🍉', '🍍', '🥭'];
+    icons = [...icons, ...icons];
+    icons.shuffle(Random());
+    _cards = List.generate(icons.length, (index) => CardModel(icons[index]));
+    notifyListeners();
+  }
 
-  final String title;
+  List<CardModel> get cards => _cards;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  void selectCard(CardModel card) async {
+    if (_isProcessing || card.isMatched || card.isFaceUp) return;
+
+    card.flip();
+    notifyListeners();
+
+    if (_firstSelected == null) {
+      _firstSelected = card;
+    } else {
+      _secondSelected = card;
+      _isProcessing = true;
+      await Future.delayed(Duration(milliseconds: 800));
+      if (_firstSelected!.icon == _secondSelected!.icon) {
+        _firstSelected!.isMatched = true;
+        _secondSelected!.isMatched = true;
+      } else {
+        _firstSelected!.flip();
+        _secondSelected!.flip();
+      }
+      _firstSelected = null;
+      _secondSelected = null;
+      _isProcessing = false;
+      notifyListeners();
+    }
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CardModel {
+  final String icon;
+  bool isFaceUp = false;
+  bool isMatched = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  CardModel(this.icon);
+
+  void flip() {
+    if (!isMatched) {
+      isFaceUp = !isFaceUp;
+    }
   }
+}
+
+class CardMatchingGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Card Matching Game")),
+      body: Center(
+        child: Consumer<GameState>(
+          builder: (context, gameState, child) {
+            return GridView.builder(
+              padding: EdgeInsets.all(16),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+              ),
+              itemCount: gameState.cards.length,
+              itemBuilder: (context, index) {
+                return CardWidget(card: gameState.cards[index]);
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  final CardModel card;
+
+  CardWidget({required this.card});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return GestureDetector(
+      onTap: () {
+        Provider.of<GameState>(context, listen: false).selectCard(card);
+      },
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 500),
+        transitionBuilder: (Widget child, Animation<double> animation) {
+          return RotationYTransition(turns: animation, child: child);
+        },
+        child:
+            card.isFaceUp || card.isMatched
+                ? Container(
+                  key: ValueKey(card.icon),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 239, 194, 194),
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(
+                      color: const Color.fromARGB(255, 33, 243, 37),
+                      width: 3,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 4,
+                        offset: Offset(2, 2),
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text(card.icon, style: TextStyle(fontSize: 40)),
+                  ),
+                )
+                : Container(
+                  key: ValueKey("back"),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 53, 178, 232),
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 4,
+                        offset: Offset(2, 2),
+                      ),
+                    ],
+                  ),
+                ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class RotationYTransition extends StatelessWidget {
+  final Widget child;
+  final Animation<double> turns;
+
+  RotationYTransition({required this.child, required this.turns});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: turns,
+      builder: (context, child) {
+        final angle = turns.value * pi;
+        return Transform(
+          transform: Matrix4.rotationY(angle),
+          alignment: Alignment.center,
+          child: child,
+        );
+      },
+      child: child,
     );
   }
 }
